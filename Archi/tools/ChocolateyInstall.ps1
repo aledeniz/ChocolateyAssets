dcd127b3b18bd925fa3b1e6e04551c0268d46e20
Alessandro Riolo <alessandro.riolo@gmail.com>
2019-01-31 00:37:48 +0000

    Updated urls and checksum for archi 4.3.2

--- Archi/tools/ChocolateyInstall.ps1
+++ Archi/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.archimatetool.com/downloads/rel-42/Archi-Win32-Setup-4.2.exe'
-$url64      = 'https://www.archimatetool.com/downloads/rel-42/Archi-Win64-Setup-4.2.exe'
+$url        = 'https://www.archimatetool.com/downloads/4.3.2/Archi-Win32-Setup-4.3.2.exe'
+$url64      = 'https://www.archimatetool.com/downloads/4.3.2/Archi-Win64-Setup-4.3.2.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -13,9 +13,9 @@ $packageArgs = @{
 
   softwareName  = 'archi'
 
-  checksum      = '39eb87626a26ad01aac578143cb2037e'
+  checksum      = '935C50087AFBFD27D8897AB838F4D4FF'
   checksumType  = 'md5'
-  checksum64    = '29e57834f6631b3994bebce839b4a4de'
+  checksum64    = '0B471ED7E4F7DB2F385AD26430EF3C0C'
   checksumType64= 'md5'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
